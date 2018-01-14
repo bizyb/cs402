36bda96031e8e5bf8590fb3c9db44976294d37c2
Bizuwork Melesse <bmelesse@elifsis.com>
2018-01-14 07:28:48 +0000

    Add compileable stub code for all functions

---
+++ warmup1/my402list.c
@@ -0,0 +1,91 @@
+/******************************************************************************/
+/* Important CSCI 402 usage information:                                      */
+/*                                                                            */
+/* This fils is part of CSCI 402 programming assignments at USC.              */
+/*         53616c7465645f5f2e8d450c0c5851acd538befe33744efca0f1c4f9fb5f       */
+/*         3c8feabc561a99e53d4d21951738da923cd1c7bbd11b30a1afb11172f80b       */
+/*         984b1acfbbf8fae6ea57e0583d2610a618379293cb1de8e1e9d07e6287e8       */
+/*         de7e82f3d48866aa2009b599e92c852f7dbf7a6e573f1c7228ca34b9f368       */
+/*         faaef0c0fcf294cb                                                   */
+/* Please understand that you are NOT permitted to distribute or publically   */
+/*         display a copy of this file (or ANY PART of it) for any reason.    */
+/* If anyone (including your prospective employer) asks you to post the code, */
+/*         you must inform them that you do NOT have permissions to do so.    */
+/* You are also NOT permitted to remove or alter this comment block.          */
+/* If this comment block is removed or altered in a submitted file, 20 points */
+/*         will be deducted.                                                  */
+/******************************************************************************/
+
+/*
+ * Author:      William Chia-Wei Cheng (bill.cheng@acm.org)
+ *
+ * @(#)$Id: listtest.c,v 1.2 2018/01/02 23:09:34 william Exp $
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include "cs402.h"
+
+#include "my402list.h"
+
+ int  My402ListLength(My402List* pList) {
+
+ 	return 1;
+ }
+ int  My402ListEmpty(My402List* pList) {
+
+ 	return 1;
+ }
+
+ int  My402ListAppend(My402List* pList, void* obj)  {
+
+ 	return 1;
+ }
+ int  My402ListPrepend(My402List* pList, void* obj) {
+
+ 	return 1;
+ }
+ void My402ListUnlink(My402List* pList, My402ListElem* elem) {
+
+ }
+ void My402ListUnlinkAll(My402List* pList) {
+
+ }
+ int  My402ListInsertAfter(My402List* pList, void* obj, My402ListElem* elem) {
+
+ 	return 1;
+ }
+ int  My402ListInsertBefore(My402List* pList, void* obj, My402ListElem* elem) {
+
+ 	return 1;
+ }
+
+ My402ListElem *My402ListFirst(My402List* pList) {
+
+ 	return NULL;
+ }
+ My402ListElem *My402ListLast(My402List* pList) {
+
+ 	return NULL;
+ }
+ My402ListElem *My402ListNext(My402List* pList, My402ListElem* elem) {
+
+ 	return NULL;
+ }
+ My402ListElem *My402ListPrev(My402List* pList, My402ListElem* elem) {
+
+ 	return NULL;
+ }
+
+ My402ListElem *My402ListFind(My402List* pList, void* obj) {
+
+ 	return NULL;
+ }
+
+ int My402ListInit(My402List* pList) {
+
+ 	return 1;
+ }
