6a89dadd87c42e01f28490686e1fbe7171bb5e92
Bizuwork Melesse <bmelesse@elifsis.com>
2018-01-30 08:53:15 +0000

    Fix seg fault in server thread packet transmission

--- warmup2/server_thread.c
+++ warmup2/server_thread.c
@@ -6,15 +6,71 @@
 #include "cs402.h"
 #include "my402list.h"
 #include "parent.h"
+#include "global.h"
+#include "arrival_thread.h"
 #include "server_thread.h"
+
+void transmitPacket(ThreadArgument *args) {
+
+	struct timeval then, now, out_q2;
+	double dTime, dTotal, sysTime;
+
+	My402ListElem *elem = My402ListFirst(args->q2);
+	Packet *packet = (Packet *) elem->obj;
+
+	My402ListUnlink(args->q2, elem);
+	(void)gettimeofday(&out_q2, NULL);
+
+	pthread_mutex_unlock(args->token_m);
+
+	packet->time_out_q2 = out_q2;
+	dTime = deltaTime(&packet->time_in_q2, &packet->time_out_q2);
+	dTotal = deltaTime(&args->epPtr->time_emul_start, &packet->time_out_q2);
+
+	printf("%012.3fms: p%d leaves Q2, time in Q2 = %.3fms\n", 
+			dTotal, packet->packetID, dTime);
+
+	(void)gettimeofday(&then, NULL);
+	dTotal = deltaTime(&args->epPtr->time_emul_start, &then);
+	packet->time_in_server = then;
+
+	int serverID = args->serverID+1;
+	int serviceTime = packet->serviceTime/THOUSAND_FACTOR;
+	printf("%012.3fms: p%d begins service at S%d, requesting %dms of service\n", 
+			dTotal, packet->packetID, serverID, serviceTime);
+
+	usleep(packet->serviceTime);
+	(void)gettimeofday(&now, NULL);
+	packet->time_out_server = now;
+
+	dTotal = deltaTime(&args->epPtr->time_emul_start, &now);
+	dTime = deltaTime(&packet->time_in_server, &packet->time_out_server);
+	sysTime = deltaTime(&packet->time_arrival, &packet->time_out_server);
+	packet->dSysTime = sysTime;
+
+	printf("%012.3fms: p%d departs from S%d, service time = %.3fms, time in system =  %.3fms\n",
+	 dTotal, packet->packetID, serverID, dTime, packet->dSysTime);
+
+	pthread_mutex_lock(args->packetList_m);
+	My402ListAppend(args->packetList, (void *)packet);
+	pthread_mutex_unlock(args->packetList_m);
+}
 
 void *server(void *obj) {
 
 	ThreadArgument *args = (ThreadArgument *) obj;
 	while(TRUE) {
 
-		//wait for wake up call here
-		// once wake up received, lock q2 with token_m
+		pthread_cond_wait(args->Q2NotEmpty, args->token_m);
+		// check again that q2 is not empty
+		if (args->q2->num_members > 0) {
+
+			transmitPacket(args);
+			
+
+		}
+		else pthread_mutex_unlock(args->token_m);
+		
 		//remove packet from q2, timestamp, serverID
 		//unlock token_m
 
